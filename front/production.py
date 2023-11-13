6aa1ddb6007c6010946ca77c410b6c39e956551e
lxeekx21 <131902846+lxeekx21@users.noreply.github.com>
2023-11-13 15:06:48 +0200

    laeeka

--- front/production.py
+++ front/production.py
@@ -2,18 +2,36 @@ import streamlit as st
 
 st.title("Cricket player predictions🏏")
 
+search_query = st.text_input("Search Player information")
+
 st.title("Weather")
-st.write("The weather predictions for today")
 
 selection = st.radio("Select the weather conditions:", ["Sunny☀️", "Overcast⛅", "Drizzle🌦️", "Rain🌧️"])
 
-# Display the selected weather condition
+
 st.write(f"Selected weather condition: {selection}")
 
-st.title("Forms.")
-st.write("See stats for your players")
+st.title("Forms")
+
+selected_form = st.slider("Select player form:", 0, 100, 50, step=10, format="%d%%")
+
+if 0 <= selected_form <=40:
+    
+    performance_category = "Struggling😭😭😭😭"
+elif 50 <= selected_form <= 70:
+    performance_category = "Solid😄😄😄😄😄"
+elif 80 <= selected_form <= 100:
+    performance_category = "World Class😁😁😁😁😁"
+else:
+    performance_category = "Unknown"
+    
+st.write(f"Selected form: {selected_form}%")
+st.write(f"Player Perfomance Category: {performance_category}")
 
 st.title("Pitch Conditions")
 st.write("Below you will find pitch conditions")
 
-search_query = st.text_input("Search Player information")
+selection = st.radio("Select the weather conditions:", ["Soft", "Dry", "Hard", "Grass"])
+st.write(f"The pitch condition is: {selection}")
+
+
